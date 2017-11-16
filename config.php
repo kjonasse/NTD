0fadb44c4c7f7de7e80b572b8971abe142a7e41d
Kimberly Jonasse <kimberlyjonasse@Kimberlys-MacBook-Air.local>
2017-11-16 16:16:25 -0500

    11th

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 //Contact form config
 //$email_to = "support@themeleaf.com";
-$email_to = "viprep@gmail.com";
+$email_to = "kimberly.jonasse@allentate.com";
 $email_subject = "Feedback form submitted";
 
 //recaptcha config
